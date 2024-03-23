b7c8843f55022312d0784292977b000482b91b1e
lamotnat <nathanl4school@gmail.com>
2024-03-23 14:18:33 -0400

    text to speech working - image to text pretty limited

--- software_side/Phase2.py
+++ software_side/Phase2.py
@@ -1,13 +1,17 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import TrOCRProcessor, VisionEncoderDecoderModel
 from PIL import Image
+import requests
 
-model_id = "vikhyatk/moondream2"
-revision = "2024-03-06"
-model = AutoModelForCausalLM.from_pretrained(
-    model_id, trust_remote_code=True, revision=revision
-)
-tokenizer = AutoTokenizer.from_pretrained(model_id, revision=revision)
+# load image from the IAM database (actually this model is meant to be used on printed text)
+# url = 'https://fki.tic.heia-fr.ch/static/img/a01-122-02-00.jpg'
+# image = Image.open(requests.get(url, stream=True).raw).convert("RGB")
+image = Image.open('software_side/road_work_cropped.png').convert("RGB")
 
-image = Image.open('<IMAGE_PATH>')
-enc_image = model.encode_image(image)
-print(model.answer_question(enc_image, "Describe this image.", tokenizer))
+processor = TrOCRProcessor.from_pretrained('microsoft/trocr-base-printed')
+model = VisionEncoderDecoderModel.from_pretrained('microsoft/trocr-base-printed')
+pixel_values = processor(images=image, return_tensors="pt").pixel_values
+
+generated_ids = model.generate(pixel_values)
+generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+
+print("Text:", generated_text)
--- software_side/Phase3.py
+++ software_side/Phase3.py
@@ -1,3 +1,4 @@
+import torch
 from transformers import pipeline
 from datasets import load_dataset
 import soundfile as sf
@@ -8,6 +9,7 @@ embeddings_dataset = load_dataset("Matthijs/cmu-arctic-xvectors", split="validat
 speaker_embedding = torch.tensor(embeddings_dataset[7306]["xvector"]).unsqueeze(0)
 # You can replace this embedding with your own as well.
 
+# TODO input the correct text
 speech = synthesiser("Hello, my dog is cooler than you!", forward_params={"speaker_embeddings": speaker_embedding})
 
 sf.write("speech.wav", speech["audio"], samplerate=speech["sampling_rate"])
