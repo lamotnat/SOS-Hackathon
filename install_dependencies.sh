d254ed85cd06e4cc2c6243ad0b78d9bacb41b97d
Aidan <aidtomjohn624@gmail.com>
2024-03-23 23:04:06 -0400

    should add a working shell file

---
+++ install_dependencies.sh
@@ -0,0 +1,6 @@
+py -m pip install --upgrade pip
+pip install transformers
+pip install --upgrade transformers sentencepiece datasets[audio]
+pip install pyaudio
+pip install pillow
+pip install torch
