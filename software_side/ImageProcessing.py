31ec37e9bfb284b50d16d8b01dc989dc3c0e684a
lamotnat <nathanl4school@gmail.com>
2024-03-23 20:42:25 -0400

    trying some stuff out

--- software_side/ImageProcessing.py
+++ software_side/ImageProcessing.py
@@ -1,10 +1,12 @@
 import Phase1, Phase2, Phase3
 from PIL import Image
 
-image = Image.open("road_work.png").convert("RGB")
+image = Image.open("practice_images/road_work.png").convert("RGB")
+
+# print(Phase1.is_street_sign(image))
 
 if Phase1.is_street_sign(image):
     raw_text = Phase2.image_to_json(image)
     clean_text = Phase2.json_to_string(raw_text)
-    
+
     Phase3.string_to_wav(clean_text)
