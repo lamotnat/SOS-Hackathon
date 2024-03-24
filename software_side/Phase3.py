acc370febdc775c2ff34f0aad1514a3c12b8bc6f
lamotnat <nathanl4school@gmail.com>
2024-03-23 20:54:41 -0400

    try one for autoplay

--- software_side/Phase3.py
+++ software_side/Phase3.py
@@ -2,8 +2,9 @@ import torch
 from transformers import pipeline
 from datasets import load_dataset
 import soundfile as sf
-from pydub import pydub.AudioSegment
+from pydub import AudioSegment
 from pydub.playback import play
+
 
 def string_to_wav(text):
     synthesiser = pipeline("text-to-speech", "microsoft/speecht5_tts")
@@ -18,5 +19,6 @@ def string_to_wav(text):
 
     sf.write("speech.wav", speech["audio"], samplerate=speech["sampling_rate"])
 
+
 def play_wav():
-    play(AudioSegment.from_file(file = "speech.wav", format = "wav"))
+    play(AudioSegment.from_file(file="speech.wav", format="wav"))
--- software_side/audio.py
+++
@@ -1,4 +0,0 @@
-from pydub import pydub.AudioSegment
-from pydub.playback import play
-
-play(AudioSegment.from_file(file = "speech.wav", format = "wav"))
